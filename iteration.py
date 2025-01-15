edab7dd64c107f76b249b36aafebed1576ba02ac
Sonia Sabbahi <soniadevpro@gmail.com>
2025-01-15 23:09:23 +0100

    update iteration py
    for else, comprehension de liste, fonctions any et all

--- iteration.py
+++ iteration.py
@@ -33,3 +33,36 @@ for element in liste:
     if element.isalpha():
         break
     print(element)
+
+
+
+# boucle for else
+
+
+prenoms = [ "Paul", "Pierre", "Jacques" ]
+for prenom in prenoms:
+    if prenom == "Jean-Jacques":
+        print("Jean est dans la liste")
+        break
+else:
+    print("Jean n'est pas dans la liste")
+
+
+
+
+# comprehension de liste
+
+
+
+liste = [ -1, -2, 3, 4, 5 ]
+liste_nb_positifs = [ i * 2 for i in liste if i > 0 ]
+
+print(liste_nb_positifs)
+
+
+# Les fonctions any() et all() permettent de vérifier si au moins un élément d'une liste est vrai ou si tous les éléments d'une liste sont vrais.
+
+
+liste = [ 0, 1, 2, 3, 4 ]
+print(any(liste)) # True
+print(all(liste)) # False
