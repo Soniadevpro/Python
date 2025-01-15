3274a2add697bee9e06b961aaf256015ad7fccb9
Sonia Sabbahi <soniadevpro@gmail.com>
2025-01-15 14:16:23 +0100

    Create calculatrice.py

---
+++ calculatriceNv1/calculatrice.py
@@ -0,0 +1,18 @@
+
+premierNombre = input(" Veuillez entrer un premier nombre : ")
+deuxiemeNombre = input(" Veuillez entrer un deuxième nombre : ")
+operateur = input(" Veuillez entrer un opérateur : ")
+
+if operateur == "+":
+    resultat = int(premierNombre) + int(deuxiemeNombre)
+elif operateur == "-":
+    resultat = int(premierNombre) - int(deuxiemeNombre)
+elif operateur == "*":
+    resultat = int(premierNombre) * int(deuxiemeNombre)
+elif operateur == "/":
+    resultat = int(premierNombre) / int(deuxiemeNombre)
+else:
+    resultat = "Erreur"
+
+
+print("Le résultat est : ", resultat)
