ce7b9909ccb26000a12e13f485bd35115ab8a3fd
Sonia Sabbahi <soniadevpro@gmail.com>
2025-01-15 22:48:06 +0100

    Create iteration

---
+++ iteration.py
@@ -0,0 +1,26 @@
+# for in 
+
+for i in [ 88, 1, 8, 5, 14 ]:
+    print(i)
+
+
+for letter in "hello":
+        print(letter)
+
+for i in range(10):
+    print("hello")
+
+
+# boucle while
+
+
+i = 0
+while i < 10:
+    print("bonjour")
+    i += 1
+
+
+continuer = "o"
+while continuer == "o":
+    print("on continue...")
+    continuer = input("voulez vous continuer ? o/n : ")
