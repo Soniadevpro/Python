973e87b6817729bad9df592129a16b3cb8e41bb2
Sonia Sabbahi <soniadevpro@gmail.com>
2025-01-21 11:20:52 +0100

    Update exercice correction

--- Exercices/resume.py
+++ Exercices/resume.py
@@ -1,37 +1,40 @@
-#Résumé exercice 25
-
-
-# Création d'une liste comprise entre 0 et 100 ses valeurs vont de -10 à + 40 cela dois afficher une alerte quand dans cette liste il y a + de 2 valeur négative "alerte ça caille" quand on a plus de 4 valeurs positives cad +20 degrés "alerte ça chauffe" sinon "la température est normale"
-
-# Création d'une liste de 100 valeurs comprises entre -10 et 40
 import random
-liste = [random.randint(-10, 40) for i in range(100)]
-print(liste)
 
-# Comptage des valeurs négatives
+temp = []
+liste_valeurs_positives = []
+liste_valeurs_negatives = []
+liste_valeurs_normales = []
 
-compteur_negatif = 0
-compteur_positif = 0
+for i in range(100):
+    temp.append(random.randint(-10,41))
 
-for i in liste:
-    if i < 0:
-        compteur_negatif += 1
-    elif i > 20:
-        compteur_positif += 1
+temp.sort()
 
-if compteur_negatif > 2:
-    print("Alerte ça caille")
-elif compteur_positif > 4:
-    print("Alerte ça chauffe")
-# else:
-#     print("La température est normale")
+for i in temp:
+    if(i<0):
+        liste_valeurs_negatives.append(i)
+    elif(0 <= i < 20):
+        liste_valeurs_normales.append(i)
+    else:
+        liste_valeurs_positives.append(i)
 
-# Affichage du résultat
+if(len(liste_valeurs_negatives) > 2):
+    print("Alerte : Attention ca caille")
 
-print(compteur_negatif)
-print(compteur_positif)
+elif(len(liste_valeurs_positives) > 4):
+    print("Alerte : Attention met de la crème")    
 
-#moyenne liste negative moyenne liste positive
+temp_min = temp[0]
+temp_max = liste_valeurs_positives[-1]
+
+print(f"Temperature maximale : {temp_max}°")
+print(f"Temperature minimale : {temp_min}°")
+print("--------------------------------------------------------")        
+print(f"Valeur de la liste positive : {liste_valeurs_positives}")
+print(f"Valeur de la liste négative : {liste_valeurs_negatives}")
+print(f"Valeur de la liste normale : {liste_valeurs_normales}")
+
+
 
 
 
