72cf90e592ce2082ec5cb4f274d0aff77dc04801
Sonia Sabbahi <soniadevpro@gmail.com>
2025-01-26 00:45:59 +0100

    update

--- Cours/heritage/heritage.py
+++ Cours/heritage/heritage.py
@@ -1,25 +1,15 @@
 import string
 
-
 class Alphabet_majuscule(object):
     """Classe mère"""
     def __init__(self):
         self.lettres_maj = string.ascii_uppercase
-        # équivalent à une chaîne reprenant tout l'alphabet avec la méthode upper()
-
-# class Alphabet_majuscule(object): vous construisez votre classe mère comme une classe normale et ordinaire
-#self.lettres = string.ascii_uppercase importation de la constante "string.ascii_uppercase" du module "string" qui est l'alphabet
-
 
 class Alphabet_minuscule(Alphabet_majuscule):
     """Classe fille"""
     def __init__(self):
         Alphabet_majuscule.__init__(self)
         self.lettres_min = self.lettres_maj.lower()
-
-
-#class Alphabet_minuscule(Alphabet_majuscule): en paramètre de la classe fille, le nom de la classe mère.
-#Alphabet_majuscule.__init__(self) dans le constructeur de la classe fille, importation du constructeur de la classe mère avec ses attributs
 
 test = Alphabet_majuscule()
 print(test.lettres_maj)
@@ -30,11 +20,11 @@ print(test2.lettres_min)
 
 print(test2.lettres_maj)
 
-
 class Alphabet_tri(Alphabet_minuscule):
     # classe petite fille
     def __init__(self):
         Alphabet_minuscule.__init__(self)
+        self.lettres = self.lettres_min  # Add this line to define self.lettres
         self.voyelles = []
         self.consonnes = []
         for lettre in self.lettres:
@@ -47,6 +37,7 @@ class Alphabet_tri(Alphabet_minuscule):
         self.voyelles_chaine = "".join(self.voyelles) 
         self.consonnes_chaine = "".join(self.consonnes)
 
+# Test the classes
 test3 = Alphabet_tri()
 print(test3.lettres) 
 print(test3.voyelles)
@@ -54,5 +45,4 @@ print(test3.consonnes)
 
 test3.listes_vers_chaine()
 print(test3.voyelles_chaine)
-test3.consonnes_chaine()
 print(test3.consonnes_chaine)
